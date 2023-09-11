435c23e2650ee510f77c2779f01ee7575dd1e282
thomaskovoor <thomaskovoor123@gmail.com>
2023-09-11 23:05:30 +0530

    Using ModelAndView and RequestParam annotation

--- SpringBootApp-1/src/main/java/com/thomas/demo/HomeController.java
+++ SpringBootApp-1/src/main/java/com/thomas/demo/HomeController.java
@@ -2,19 +2,24 @@ package com.thomas.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class HomeController {
+	
 	@RequestMapping("/home")
-	public String home(HttpServletRequest req) {
+	public ModelAndView home(@RequestParam("name") String myName) {
+
 		
-		String name = req.getParameter("name");
-		HttpSession session = req.getSession();
-		session.setAttribute("name", name);
-		return "home";
+		ModelAndView mv = new ModelAndView();
+		
+		mv.addObject("name",myName);
+		mv.setViewName("home");
+		return mv;
 	}
 
 }
